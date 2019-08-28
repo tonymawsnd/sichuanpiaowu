445f5b38b8c2336d29a3a9f3f95f4967b734f263
xinghaihao <1164690951@qq.com>
2019-08-28 15:32:13 +0800

    已实现登录功能，注册功能，其他的尚未实现

---
+++ src/main/java/cn/scpw/controller/ZhiFuController.java
@@ -0,0 +1,31 @@
+package cn.scpw.controller;
+
+import cn.scpw.pojo.Checi;
+import cn.scpw.pojo.User;
+import cn.scpw.service.UserService;
+import cn.scpw.service.ZhiFuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+
+/**
+ * @Author XHH
+ * @Date 2019/8/28 14.22
+ * 这是票务支付的Controller
+ */
+@Controller
+public class ZhiFuController {
+    @Autowired
+    ZhiFuService zhiFuService;
+
+    @RequestMapping(value = "/isZhiFu", method = RequestMethod.POST, produces = {"application/json;charset=UTF-8"})
+    @ResponseBody
+    public void isZhiFu(@RequestBody(required = false) Map<String,String> map){
+        return ZhiFuService.isZhiFu(map);
+    }
+}
---
+++ src/main/java/cn/scpw/service/ZhiFuService.java
@@ -0,0 +1,52 @@
+package cn.scpw.service;
+
+import cn.scpw.pojo.*;
+import cn.scpw.util.AliPayFiled;
+import cn.scpw.util.AliPayUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author XHH
+ * @Date 2019/8/28 14:25
+ * 支付Service
+ */
+
+@Service
+public class ZhiFuService {
+
+        @Autowired
+        DingdanMapper dingdanMapper;
+
+        /**
+         * 取出前段传递的车次价格等参数，调用AliPayUtil完成支付
+         * @param map 发起订单的人的userid封装对象
+         * @return
+         */
+        @Transactional(rollbackFor = Exception.class)
+        public String isZhiFu(Map<String,String> map){
+
+            //根据sessionStorage调取买票人的id
+            //select查询车票的信息和状态
+            //完成支付
+
+            Dingdan dingDan = dingdanMapper.selectAllMyDingDanByUserId(map.get("userid"));
+
+            AliPayUtil aliPayUtil = new AliPayUtil();
+            return aliPayUtil.aliPay(
+                    "http://localhost:8080/success.html",
+                    AliPayFiled.FAST_INSTANT_TRADE_PAY,
+                    dingDan.getId(),
+                    "1",
+                    dingDan.getCheci().getCheci(),
+                    "火车票");
+
+
+        }
+
+}
